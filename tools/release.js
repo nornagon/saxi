ac9247e9383890f7de60274c12b40c3018b89213
Jeremy Apthorp <nornagon@nornagon.net>
2019-01-27 11:16:42 -0800

    chore: add release helper script

---
+++ tools/release.js
@@ -0,0 +1,35 @@
+const semver = require('semver')
+const path = require('path')
+const fs = require('fs')
+const child_process = require('child_process')
+
+const repositoryRoot = path.join(__dirname, '..')
+const packageJsonPath = path.join(repositoryRoot, 'package.json')
+const package = JSON.parse(fs.readFileSync(packageJsonPath))
+
+const args = require('yargs')
+  .strict()
+  .option("level", {
+    type: "string",
+    choices: ["major", "premajor", "minor", "preminor", "patch", "prepatch", "prerelease"],
+    demandOption: true
+  })
+  .parse()
+
+const newVersion = semver.inc(package.version, args.level)
+
+const newPackageJson = {...package, version: newVersion}
+fs.writeFileSync(packageJsonPath, JSON.stringify(newPackageJson, null, 2))
+
+function git(args) {
+  const r = child_process.spawnSync("git", args, {cwd: repositoryRoot})
+  if (r.status !== 0) {
+    console.error(r.stderr)
+    throw new Error(`Command failed: git ${args.join(" ")}`)
+  }
+}
+git(["add", "package.json"])
+git(["commit", "-m", `bump to ${newVersion}`])
+git(["tag", `v${newVersion}`])
+
+console.log(`Bumped version from ${package.version} -> ${newVersion}`)
