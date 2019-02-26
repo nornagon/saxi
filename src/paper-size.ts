11d32d2cefda1495bf4c7be4d9a223494885bb1a
Jeremy Apthorp <nornagon@nornagon.net>
2019-02-25 22:07:18 -0800

    round standard paper sizes to fix 304.799999

--- src/paper-size.ts
+++ src/paper-size.ts
@@ -1,5 +1,9 @@
 import {Vec2, vmul} from "./vec";
 
+function vround(v: Vec2, digits: number = 2): Vec2 {
+  return { x: Number(v.x.toFixed(digits)), y: Number(v.y.toFixed(digits)) }
+}
+
 export class PaperSize {
 
   get landscape(): PaperSize {
@@ -17,14 +21,14 @@ export class PaperSize {
   }
 
   public static standard: {[name: string]: PaperSize} = {
-    "USLetter": new PaperSize(vmul({x: 8.5, y: 11}, 25.4)),
-    "USLegal": new PaperSize(vmul({x: 8.5, y: 14}, 25.4)),
-    "ArchA": new PaperSize(vmul({x: 9, y: 12}, 25.4)),
+    "USLetter": new PaperSize(vround(vmul({x: 8.5, y: 11}, 25.4))),
+    "USLegal": new PaperSize(vround(vmul({x: 8.5, y: 14}, 25.4))),
+    "ArchA": new PaperSize(vround(vmul({x: 9, y: 12}, 25.4))),
     "A4": new PaperSize({x: 210, y: 297}),
     "A5": new PaperSize({x: 148, y: 210}),
     "A6": new PaperSize({x: 105, y: 148}),
-    "6x8": new PaperSize(vmul({x: 6, y: 8}, 25.4)),
-    "5x7": new PaperSize(vmul({x: 5, y: 7}, 25.4)),
+    "6x8": new PaperSize(vround(vmul({x: 6, y: 8}, 25.4))),
+    "5x7": new PaperSize(vround(vmul({x: 5, y: 7}, 25.4))),
   };
   public size: Vec2;
   constructor(size: Vec2) {
