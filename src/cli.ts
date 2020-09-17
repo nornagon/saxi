83aa8eae68a82569110c51e995b090e45f711e9f
Jeremy Rose <nornagon@nornagon.net>
2020-09-16 17:45:52 -0700

    wip: cli-driven plotting

--- src/cli.ts
+++ src/cli.ts
@@ -1,40 +1,99 @@
 import yargs from "yargs";
 import {connectEBB, startServer} from "./server";
+import {plan, AxidrawFast} from "./planning";
+import {Window} from "svgdom";
+import * as fs from "fs";
+import {flattenSVG} from "flatten-svg";
+import { Vec2 } from "./vec";
+import { formatDuration } from "./util";
+
+function parseSvg(svg: string) {
+  const window = new Window
+  window.document.documentElement.innerHTML = svg
+  return window.document.documentElement
+}
 
 export function cli(argv: string[]): void {
-  const args = yargs.strict()
-    .option("port", {
-      alias: "p",
-      default: Number(process.env.PORT || 9080),
-      describe: "TCP port on which to listen",
-      type: "number"
-    })
+  yargs.strict()
     .option("device", {
       alias: "d",
       describe: "device to connect to",
       type: "string"
     })
-    .option("enable-cors", {
-      describe: "enable cross-origin resource sharing (CORS)",
-      type: "boolean"
-    })
-    .option("firmware-version", {
-      describe: "print the device's firmware version and exit",
-      type: "boolean"
-    })
-    .parse(argv);
-
-  if (args["firmware-version"]) {
-    connectEBB(args.device).then(async (ebb) => {
-      if (!ebb) {
-        console.error(`No EBB connected`);
-        return process.exit(1);
+    .command('$0', 'run the saxi web server',
+      yargs => yargs
+        .option("port", {
+          alias: "p",
+          default: Number(process.env.PORT || 9080),
+          describe: "TCP port on which to listen",
+          type: "number"
+        })
+        .option("enable-cors", {
+          describe: "enable cross-origin resource sharing (CORS)",
+          type: "boolean"
+        })
+        .option("firmware-version", {
+          describe: "print the device's firmware version and exit",
+          type: "boolean"
+        }),
+      args => {
+        if (args["firmware-version"]) {
+          connectEBB(args.device).then(async (ebb) => {
+            if (!ebb) {
+              console.error(`No EBB connected`);
+              return process.exit(1);
+            }
+            const fwv = await ebb.firmwareVersion();
+            console.log(fwv);
+            await ebb.close();
+          });
+        } else {
+          startServer(args.port, args.device, args["enable-cors"]);
+        }
       }
-      const fwv = await ebb.firmwareVersion();
-      console.log(fwv);
-      await ebb.close();
-    });
-  } else {
-    startServer(args.port, args.device, args["enable-cors"]);
-  }
+    )
+    .command('plot <file>', 'plot an svg, then exit',
+      yargs => yargs
+        .positional("file", {
+          type: 'string',
+          description: "File to plot",
+        })
+        .option("paper-size", {
+          alias: "s",
+          describe: "Paper size to use",
+          type: "boolean",
+          required: true
+        }),
+      async args => {
+        console.log("reading svg...")
+        const svg = fs.readFileSync(args.file, 'utf8')
+        console.log("parsing svg...")
+        const parsed = parseSvg(svg)
+        console.log("flattening svg...")
+        const lines = flattenSVG(parsed, {})
+        console.log("generating motion plan...")
+        const profile = AxidrawFast
+        const p = plan(linesToVecs(lines), profile)
+        console.log(`${p.motions.length} motions, estimated duration: ${formatDuration(p.duration())}`)
+        console.log("connecting to plotter...")
+        const ebb = await connectEBB(args.device)
+        if (!ebb) {
+          console.error("Couldn't connect to device!")
+          process.exit(1)
+        }
+        console.log("plotting...")
+        ebb.executePlan(p)
+        console.log("done!")
+      }
+    )
+    .parse(argv);
+}
+
+function linesToVecs(lines: any[]): Vec2[][] {
+  return lines.map((line) => {
+    const a = line.points.map(([x, y]: [number, number]) => ({x, y}));
+    (a as any).stroke = line.stroke;
+    (a as any).groupId = line.groupId;
+    return a;
+  });
 }
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,27 @@
 declare module '*.svg';
+declare module 'svgdom';
 declare module 'wake-lock';
 declare module '@serialport/binding-mock';
+
+declare module 'flatten-svg/index' {
+  export { flattenSVG } from "flatten-svg/svg-to-paths";
+
+}
+declare module 'flatten-svg/svg-to-paths' {
+  interface Options {
+    maxError: number;
+  }
+  type Point = [number, number];
+  interface Line {
+    points: Point[];
+    stroke?: string;
+    groupId?: string;
+  }
+  export function flattenSVG(svg: SVGElement, options?: Partial<Options>): Line[];
+  export {};
+
+}
+declare module 'flatten-svg' {
+  import main = require('flatten-svg/index');
+  export = main; 
+}
