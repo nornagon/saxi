0a540c30babc8066b5dd5ac7bdcf22d73c8a34ab
Alex <52292902+alexrudd2@users.noreply.github.com>
2023-10-10 12:11:58 -0500

    Disable layer select while plotting (#107)

--- src/ui.tsx
+++ src/ui.tsx
@@ -781,6 +781,7 @@ function LayerSelector({state}: {state: State}) {
         value={[...selectedLayers]}
         onChange={layersChanged}
         size={3}
+        disabled={state.progress != null}
       >
         {layers.map((layer) => <option key={layer}>{layer}</option>)}
       </select>
